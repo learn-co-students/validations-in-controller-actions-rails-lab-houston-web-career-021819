a3247418aa9f341f17ca0190ae0b61dad5642d7a
HemalathaMurugan <hema@Hemalathas-MBP.attlocal.net>
2019-03-19 23:23:19 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,12 +4,22 @@ class AuthorsController < ApplicationController
   end
 
   def new
+    #@author = Author.new
   end
 
   def create
-    @author = Author.create!(author_params)
-
-    redirect_to author_path(@author)
+    #@author = Author.create!(author_params)
+    #the above line was throwing error beacuse create itself will perform save. 
+    #but we want our database to save only if valid.
+    #so using new in below statement
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+       "Email has already been taken, Name can't be blank"
+      render :new
+    end
   end
 
   private
